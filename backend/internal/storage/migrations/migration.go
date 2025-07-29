66ee3f6fda58a471a03bdef96fcb25762d25ff7a
Dnreikronos <dnreikronos2@gmail.com>
2025-07-29 20:57:43 -0300

    feature: create migrations package

---
+++ backend/internal/storage/migrations/migration.go
@@ -0,0 +1,14 @@
+package migrations
+
+import (
+	"github.com/ProgramadoresSemPatria/HB02-2025_braindancers/models"
+	"gorm.io/gorm"
+)
+
+func RunMigration(db *gorm.DB) {
+	CreateTables(db)
+}
+
+func CreateTables(db *gorm.DB) {
+	db.AutoMigrate(&models.User{})
+}
