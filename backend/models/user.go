04dd00c5f8ebf3c927716bd9e1b088561da2a486
Dnreikronos <dnreikronos2@gmail.com>
2025-07-30 19:56:58 -0300

    refactor: change name of the struct to be RegisterInput

--- backend/models/user.go
+++ backend/models/user.go
@@ -7,7 +7,7 @@ import (
 	"gorm.io/gorm"
 )
 
-type InpuStruct struct {
+type RegisterInput struct {
 	Name     string `json:"name" binding:"required"`
 	Email    string `json:"email" binding:"required"`
 	Password string `json:"password" binding:"required"`
