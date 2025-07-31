80da68d3d17c40a4a405bd36ac96a155ed49cf4a
Pablo Montes <169383657+itspablomontes@users.noreply.github.com>
2025-07-30 21:20:19 -0300

    feat: add chat model and input

--- backend/internal/models/user.go
+++ backend/internal/models/user.go
@@ -19,20 +19,34 @@ type SignInInput struct {
 }
 
 type UserResponse struct {
-	ID        uuid.UUID `json:"id, omitempty"`
-	Name      string    `json:"name, omitempty"`
-	Email     string    `json:"email, omitempty"`
+	ID        uuid.UUID `json:"id,omitempty"`
+	Name      string    `json:"name,omitempty"`
+	Email     string    `json:"email,omitempty"`
 	UpdatedAt time.Time `json:"updated_at"`
 	CreatedAt time.Time `json:"created_at"`
 }
 
 type User struct {
-	ID        uuid.UUID `json:"id" gorm:"type:uuid;primaryKey;`
+	ID        uuid.UUID `json:"id" gorm:"type:uuid;primaryKey;"`
 	Name      string    `json:"name"`
 	Email     string    `json:"email"`
 	Password  string    `json:"password"`
 	UpdatedAt time.Time `json:"created_at"`
 	CreatedAt time.Time `json:"updated_at"`
+}
+
+type Chat struct {
+	ID           uuid.UUID `json:"id" gorm:"type:uuid;primaryKey;"`
+	Title        string    `json:"title" gorm:"not null"`
+	LastActivity time.Time `json:"last_activity"`
+	UserID       uuid.UUID `json:"user_id" gorm:"type:uuid;not null;index"`
+	User         User      `json:"user" gorm:"foreignKey:UserID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	UpdatedAt    time.Time `json:"updated_at" gorm:"autoCreateTime"`
+	CreatedAt    time.Time `json:"created_at" gorm:"autoUpdateTime"`
+}
+
+type ChatInput struct {
+	Title string `json:"title" binding:"required"`
 }
 
 func FilteredResponse(user User) UserResponse {
