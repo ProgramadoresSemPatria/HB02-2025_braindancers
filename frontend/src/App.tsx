8e74d11bc97ef9f96bbfbb0a87fad2b2776750cd
Vandilson Brito <108373095+vandilsonbrito@users.noreply.github.com>
2025-08-01 22:45:28 -0300

    feat: declare route for ImageUploadPage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { I18nProvider } from './contexts/i18nContext'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import HomePage from './pages/Home'
 import ResultPage from './pages/ResultPage'
+import ImageUploadPage from './pages/UploadImagePage'
 import Header from './components/Header'
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/upload" element={<ImageUploadPage />} />
           <Route path="/result" element={<ResultPage />} />
         </Routes>
       </I18nProvider>
