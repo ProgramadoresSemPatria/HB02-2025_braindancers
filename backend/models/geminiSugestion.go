826f1a63680a5b400498562d762989c6031c4ce7
Dnreikronos <dnreikronos2@gmail.com>
2025-08-01 21:11:43 -0300

    feature: create FashionResponse struct

--- backend/models/geminiSugestion.go
+++ backend/models/geminiSugestion.go
@@ -4,3 +4,10 @@ type GeminiSugestion struct {
 	Suggestion string `json:"suggestion"`
 	Why        string `json:"why"`
 }
+
+type FashionResponse struct {
+	Tags       []string `json:"tags"`
+	Colors     []string `json:"colors"`
+	Suggestion string   `json:"suggestion"`
+	Why        string   `json:"why"`
+}
